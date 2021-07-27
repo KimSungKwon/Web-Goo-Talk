8227cf0cb3f873e3ff2b0f34bc61d77a01347e2a
KimSungKwon <sungkwonn@naver.com>
2021-07-27 23:57:30 +0900

    Create Lobby Component

--- src/components/main/Lobby.js
+++ src/components/main/Lobby.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import RoomList from '../rooms/RoomList'
+import styled from 'styled-components'
+import CreateRoom from './CreateRoom';
+
+const LobbyBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+        display:flex;
+        align-self: center;
+    }
+`;
 
 const Lobby = () => {
     return (
-        <div>
-            Lobby
-        </div>
+        <LobbyBlock>
+            <CreateRoom />
+            <h1>현재 참여 중인 대화방</h1>
+            <RoomList />
+        </LobbyBlock>
     )
 }
 
