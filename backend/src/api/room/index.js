c0647e5b28aa3ed5c0c2720f5ae9b8d959c746fa
KimSungKwon <sungkwonn@naver.com>
2021-08-04 23:39:58 +0900

    Create Request Validation Middleware

--- backend/src/api/room/index.js
+++ backend/src/api/room/index.js
@@ -5,8 +5,8 @@ const room = new Router();
 
 room.post('/', roomCtrl.create);    // 대화방 생성
 room.get('/', roomCtrl.list);       // 대화방 목록 조회
-room.get('/:id', roomCtrl.enter);   // 특정 대화방 입장
-room.delete('/:id', roomCtrl.remove);   // 특정 대화방 삭제
+room.get('/:id', roomCtrl.checkObjectId, roomCtrl.enter);   // 특정 대화방 입장
+room.delete('/:id', roomCtrl.checkObjectId, roomCtrl.remove);   // 특정 대화방 삭제
 
 
 
--- backend/src/api/room/room.ctrl.js
+++ backend/src/api/room/room.ctrl.js
@@ -1,4 +1,18 @@
 import Room from '../../models/room';
+import mongoose from 'mongoose';
+import Joi from '@hapi/joi';
+
+const { ObjectId } = mongoose.Types;
+
+// 요청 검증 미들웨어
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400;   // Bad Request
+        return ;
+    }
+    return next();
+};
 
 /*
     대화방 생성
@@ -6,6 +20,21 @@ import Room from '../../models/room';
     { title, body }
 */
 export const create = async ctx => {
+    
+    // 요청 내용 검증
+    const schema = Joi.object().keys({
+        title: Joi.string().required(),
+        body: Joi.string().required(),
+        users: Joi.array().items(Joi.string()).required()
+    });
+
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;   // Bad Request
+        ctx.body = result.error;
+        return;
+    }
+
     const { title, body, users } = ctx.request.body;
     const room = new Room({
         title,
@@ -26,8 +55,17 @@ export const create = async ctx => {
 */
 export const list = async ctx => {
     try {
-        const room = await Room.find().exec();
-        ctx.body = room;
+        const rooms = await Room.find().sort({ _id: -1 }).exec();    // reverse order
+        ctx.body = rooms;
+    
+        // body의 길이가 100자 이상이면 내용 자르기
+        ctx.body = rooms
+            .map(room => room.toJSON())
+            .map(room => ({
+                ...room,
+                body: room.body.length < 100 ? room.body : `${room.body.slice(0, 100)}...`
+            }))
+
     } catch (e) {
         ctx.throw(500, e);
     }
