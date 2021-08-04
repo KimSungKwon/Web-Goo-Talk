1a81f3d1da3f741a33d0567e1d48fcb03edded1d
KimSungKwon <sungkwonn@naver.com>
2021-08-04 23:55:18 +0900

    Routing auth api

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,8 +1,10 @@
 import Router from 'koa-router';
 import room from './room';
+import auth from './auth';
 
 const api = new Router();
 
 api.use('/room', room.routes());
+api.use('/auth', auth.routes());
 
 export default api;
