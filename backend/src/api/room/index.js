bfa96f04a3a243b3cd2b2a386c61ad8768c2ba3d
KimSungKwon <sungkwonn@naver.com>
2021-08-08 00:37:55 +0900

    Create checkLoggedIn MiddleWare

--- backend/src/api/room/index.js
+++ backend/src/api/room/index.js
@@ -1,12 +1,13 @@
 import Router from 'koa-router';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 import * as roomCtrl from './room.ctrl';
 
 const room = new Router();
 
-room.post('/', roomCtrl.create);    // 대화방 생성
-room.get('/', roomCtrl.list);       // 대화방 목록 조회
-room.get('/:id', roomCtrl.checkObjectId, roomCtrl.enter);   // 특정 대화방 입장
-room.delete('/:id', roomCtrl.checkObjectId, roomCtrl.remove);   // 특정 대화방 삭제
+room.post('/', checkLoggedIn, roomCtrl.create);    // 대화방 생성
+room.get('/', checkLoggedIn, roomCtrl.list);       // 대화방 목록 조회
+room.get('/:id', checkLoggedIn, roomCtrl.checkObjectId, roomCtrl.enter);   // 특정 대화방 입장
+room.delete('/:id', checkLoggedIn, roomCtrl.checkObjectId, roomCtrl.remove);   // 특정 대화방 삭제
 
 
 
